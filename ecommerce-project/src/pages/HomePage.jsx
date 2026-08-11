7a001bf4d83e27df47575df919671ee7365355b2
BananaCode2023 <peter.john.reyes0326@gmail.com>
2026-08-11 21:13:09 +0800

    Genereate HTML for the cart items

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import HomeFavicon from '../assets/images/home-favicon.png'
+import { formatMoney } from '../utils/money';
 
 export function HomePage({cart}) {
     
@@ -52,7 +53,7 @@ export function HomePage({cart}) {
                                 </div>
 
                                 <div className="product-price">
-                                    ${(product.priceCents / 100).toFixed(2)}
+                                    ${formatMoney(product.priceCents)}
                                 </div>
 
                                 <div className="product-quantity-container">
